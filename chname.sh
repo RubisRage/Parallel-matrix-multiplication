d711f9bf019ce51452b9384aa5b3e18525a41f81
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2021-05-16 20:23:58 +0100

    Added more data for benchmarking. Minor changes

--- chname.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-for name in $@;do
-    mv $name/* $name/$name.json
-done
--- launch_benchmark.sh
+++ launch_benchmark.sh
@@ -4,30 +4,37 @@
 
 
 
-tests="test1.data test2.data test3.data test4.data" # test3.data test4.data"
+# tests="test1 test2 test3 test4" # test3.data test4.data"
+tests="test250 test500 test750 test1 test2 test3 test4"
 
-for t in $tests;do
-    echo output/seq-${t:0:5}-3
-    ./seq input/$t
-    mv papi_hl_output output/seq-${t:0:5}-3
-done
+for n in 4;do
+    for t in $tests;do
+        echo output/seq/$t/seq-$t-$n.json
+        ./seq input/$t.data
+        mv papi_hl_output/* output/seq/$t/seq-$t-$n.json
+        rmdir papi_hl_output
+    done
 
-for t in $tests;do
-    echo output/omp-${t:0:5}-3
-    ./omp input/$t
-    mv papi_hl_output output/omp-${t:0:5}-3
-done
+    for t in $tests;do
+        echo output/omp/$t/omp-$t-$n.json
+        ./omp input/$t.data
+        mv papi_hl_output/* output/omp/$t/omp-$t-$n.json
+        rmdir papi_hl_output
+    done
 
-for t in $tests;do
-    echo output/cuda-${t:0:5}-2
-    ./cuda input/$t
-    mv papi_hl_output output/cuda-${t:0:5}-2
-done
+    for t in $tests;do
+        echo output/cuda/$t/cuda-$t-$n.json
+        ./cuda input/$t.data
+        mv papi_hl_output/* output/cuda/$t/cuda-$t-$n.json
+        rmdir papi_hl_output
+    done
 
-for t in $tests;do
-    echo output/mpi-${t:0:5}-2
-    mpirun -n 12 --hostfile ./hostfile ~/cloud/mpi input/$t -v
-    mv papi_hl_output output/mpi-${t:0:5}-2
+    for t in $tests;do
+        echo output/mpi/$t/mpi-$t-$n.json
+        mpirun -n 12 --hostfile ./hostfile ~/cloud/mpi input/$t.data -v
+        mv papi_hl_output/*0.json output/mpi/$t/mpi-$t-$n.json
+        rm -R papi_hl_output
+    done
 done
 
 # spd-say hello
