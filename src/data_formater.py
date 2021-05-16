bea0cab0c5eb4a0dad4adbe0d6d1fc47e0b7782b
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2021-05-16 17:49:41 +0100

    Restructured output folder, started performance study.

--- src/data_formater.py
+++ src/data_formater.py
@@ -1,43 +1,51 @@
 #!/bin/python3
 
-import sys
 import json
 import os
+import pandas as pd
+import numpy as np
 
 attributes = ("cycles",
               "real_time_nsec",
               "PAPI_TOT_INS",
               "PAPI_TOT_CYC")
 
-program = ("seq",
-           "omp",
-           "cuda",
-           "mpi")
+dirs = ("seq",
+        "omp",
+        "cuda",
+        "mpi")
+
+subdirs = ("test1", "test2", "test3", "test4")
+
+def fill(df: pd.DataFrame):
+
+    for d in dirs:
+        for sd in subdirs:
+            for root,_,files in os.walk(os.path.join("output",d,sd)):
+                for f in files:
+                    ind = f.split("-")
+                    with open(root+"/"+f) as js:
+                         data = json.load(js)["threads"]["0"]["regions"]["0"]
+                    for a in attributes:
+                        df.loc[ind[0], a][ind[1]][ind[2][0]] = data[a]
 
 
-for p in program:
-        for t in range(1,5):
-            print("output/"+p+f"-test{t}/"+p+f"-test{t}.json")
-            # with open("output/"+p+f"-test{t}/"+p+f"-test{t}.json") as f:
-            #     data = json.load(f)["threads"]["0"]["regions"]["0"]
-            #     for a in attributes:
-            #         print()
+def main():
 
-# for root, dirs, _ in os.walk("output"):
-#     for d in dirs:
-#         for _,_,files in os.walk(os.path.join(root,d)):
-#             for f in files:
-#                 print(os.path.join(root,d,f))
+    col = ["test" + str(i) for i in range(1,5)]
+    scol = [str(i) for i in range(1,4)]
+    row = dirs
+    srow = attributes
 
-# with open("output/seq-test1/seq-test1.json", "r") as f:
-#     data = json.load(f)["threads"]["0"]["regions"]["0"]
+    shape = (len(srow)*len(row),len(col)*len(scol))
 
-#     for item in data.items():
-#         print(item[1])
+    mcol = pd.MultiIndex.from_product([col,scol])
+    mrow = pd.MultiIndex.from_product([row,srow])
 
-# with open("output/seq-test1/seq-test1.csv", "w") as f:
-#     print
-#     pass
+    df = pd.DataFrame(np.full(shape, -1), index=mrow, columns=mcol)
+    fill(df)
 
-#print(data)
-#
+    np.savetxt("results.csv", df.values,delimiter=',')
+
+if __name__=="__main__":
+    main()
