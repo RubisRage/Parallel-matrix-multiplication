7188858b9d231517d9529ca04a26f5a449833f7d
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2021-06-23 19:16:00 +0100

    Deadline final version.

--- utils/launch_benchmark.sh
+++ utils/launch_benchmark.sh
@@ -9,60 +9,52 @@ cp mpi_br ~/cloud
 cp mpi ~/cloud
 
 for n in {1..7};do
-#   for t in $tests;do
-#       echo output/seq/$t/seq-$t-$n.json
-#       ./seq input/$t.data
-#       mkdir -p output/seq/$t/
-#       mv papi_hl_output/* output/seq/$t/seq-$t-$n.json
-#       rmdir papi_hl_output
-#   done
-
-#   for t in $tests;do
-#       echo output/omp/$t/omp-$t-$n.json
-#       ./omp input/$t.data
-#       mkdir -p output/omp/$t/
-#       mv papi_hl_output/* output/omp/$t/omp-$t-$n.json
-#       rmdir papi_hl_output
-#   done
-
-#   for t in $tests;do
-#       echo output/cuda/$t/cuda-$t-$n
-#       ./cuda input/$t.data
-#       mkdir -p output/cuda/$t/
-#       mv papi_hl_output/* output/cuda/$t/cuda-$t-$n.json
-#       rmdir papi_hl_output
-#   done
-
-#   for t in $tests;do
-#       echo output/mpi/$t/mpi-$t-$n.json
-#       mpirun -n 12 --hostfile ./hostfile ~/cloud/mpi input/$t.data -v
-#       mkdir -p output/mpi/$t/
-#       mv papi_hl_output/*0.json output/mpi/$t/mpi-$t-$n.json
-#       rm -R papi_hl_output
-#   done
-
-
-#   for t in $tests;do
-#       echo output/mpi_br/$t/mpi_br-$t-$n.json
-#       mpirun -n 12 --hostfile ./hostfile ~/cloud/mpi_br input/$t.data -v
-#       mkdir -p output/mpi_br/$t/
-#       mv papi_hl_output/*0.json output/mpi_br/$t/mpi_br-$t-$n.json
-#       rm -R papi_hl_output
-#   done
-
-#   for t in $tests;do
-#       echo output/cuda_dim/$t/cuda_dim-$t-$n
-#       ./cuda_dim input/$t.data
-#       mkdir -p output/cuda_dim/$t/
-#       mv papi_hl_output/* output/cuda_dim/$t/cuda_dim-$t-$n.json
-#       rmdir papi_hl_output
-#   done
+    for t in $tests;do
+        echo output/seq/$t/seq-$t-$n.json
+        ./seq input/$t.data
+        mkdir -p output/seq/$t/
+        mv papi_hl_output/* output/seq/$t/seq-$t-$n.json
+        rmdir papi_hl_output
+    done
 
     for t in $tests;do
-        echo output/cuda_test/$t/cuda_test-$t-$n
+        echo output/omp/$t/omp-$t-$n.json
+        ./omp input/$t.data
+        mkdir -p output/omp/$t/
+        mv papi_hl_output/* output/omp/$t/omp-$t-$n.json
+        rmdir papi_hl_output
+    done
+
+    for t in $tests;do
+        echo output/cuda/$t/cuda-$t-$n
         ./cuda input/$t.data
-        mkdir -p output/cuda_test/$t/
-        mv papi_hl_output/* output/cuda_test/$t/cuda_test-$t-$n.json
+        mkdir -p output/cuda/$t/
+        mv papi_hl_output/* output/cuda/$t/cuda-$t-$n.json
+        rmdir papi_hl_output
+    done
+
+    for t in $tests;do
+        echo output/mpi/$t/mpi-$t-$n.json
+        mpirun -n 12 --hostfile ./hostfile ~/cloud/mpi input/$t.data -v
+        mkdir -p output/mpi/$t/
+        mv papi_hl_output/*0.json output/mpi/$t/mpi-$t-$n.json
+        rm -R papi_hl_output
+    done
+
+
+    for t in $tests;do
+        echo output/mpi_br/$t/mpi_br-$t-$n.json
+        mpirun -n 12 --hostfile ./hostfile ~/cloud/mpi_br input/$t.data -v
+        mkdir -p output/mpi_br/$t/
+        mv papi_hl_output/*0.json output/mpi_br/$t/mpi_br-$t-$n.json
+        rm -R papi_hl_output
+    done
+
+    for t in $tests;do
+        echo output/cuda_dim/$t/cuda_dim-$t-$n
+        ./cuda_dim input/$t.data
+        mkdir -p output/cuda_dim/$t/
+        mv papi_hl_output/* output/cuda_dim/$t/cuda_dim-$t-$n.json
         rmdir papi_hl_output
     done
 done
